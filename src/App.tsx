47226bf53e5b01ee0aff9ca71206b115c0ba5447
Z-Salimi <zahra.2002.salimi@gmail.com>
2024-10-28 23:56:09 +0330

    start

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { IndexPage } from "./components/indexPage";
+
+const queryClient = new QueryClient();
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {/* <RouterProvider router={Router}></RouterProvider> */}
+      <IndexPage />
+    </QueryClientProvider>
+
+  );
+}
+
+export default App;
